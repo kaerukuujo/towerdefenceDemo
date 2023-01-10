b83d9c1b29327fa84ee75ebe5622c603a7833663
kaerukuujo <mrjolyne@gmail.com>
2023-01-10 15:53:53 +0000

    projectiles working + basic wave functionality

--- js/index.js
+++ js/index.js
@@ -39,26 +39,78 @@ image.src = './img/gameMap.png';
 
 const enemies = [];
 
-for(let i = 1; i < 10; i++){
-    const xOffset = i * 150;
-    enemies.push(
-      new Enemy({
-        position: { x: waypoints[0].x - xOffset, y: waypoints[0].y }
-    }))
+function spawnEnemies(spawnCount){
+    for(let i = 1; i < spawnCount + 1; i++){
+        const xOffset = i * 150;
+        enemies.push(
+          new Enemy({
+            position: { x: waypoints[0].x - xOffset, y: waypoints[0].y }
+        }))
+    }
 }
 
 const buildings = [];
+let activeTile = undefined;
+let enemyCount = 3;
+
+spawnEnemies(enemyCount);
 
 function animate(){
     requestAnimationFrame(animate);
 
     c.drawImage(image, 0, 0);
-    enemies.forEach(enemy => {
+    for(let i = enemies.length -1; i >= 0; i--) {
+        const enemy = enemies[i];
         enemy.update();
-    })
+    }
 
     placementTiles.forEach(tile => {
         tile.update(mouse);
+    })
+
+    buildings.forEach(building => {
+        building.update();
+        building.target = null;
+        const validEnemies = enemies.filter(enemy => {
+            const xDifference = enemy.center.x - building.center.x;
+            const yDifference = enemy.center.y - building.center.y;
+            const distance = Math.hypot(xDifference, yDifference);
+            return distance < enemy.radius + building.radius;
+        });
+        building.target = validEnemies[0];
+
+        for(let i = building.projectiles.length -1; i >= 0; i--){
+            const projectile = building.projectiles[i];
+            projectile.update();
+
+            const xDifference = projectile.enemy.center.x - projectile.position.x;
+            const yDifference = projectile.enemy.center.y - projectile.position.y;
+            const distance = Math.hypot(xDifference, yDifference);
+
+            // when a projectile hits an enemy
+
+            if(distance < projectile.enemy.radius + projectile.radius){
+                //enemy health and enemy removal
+                projectile.enemy.health -= 20;
+                if(projectile.enemy.health <= 0) {
+                    const enemyIndex = enemies.findIndex((enemy) => {
+                        return projectile.enemy === enemy;
+                    })
+
+                    if(enemyIndex > -1) enemies.splice(enemyIndex, 1);                    
+                }
+
+                //tracking total amount of enemies
+                if (enemies.length === 0){
+                    enemyCount += 2;
+                    spawnEnemies(enemyCount);
+                };
+
+                console.log(projectile.enemy.health);
+                building.projectiles.splice(i, 1);
+            }
+            // console.log(distance);
+        }
     })
 }
 
@@ -67,9 +119,36 @@ const mouse = {
     y: undefined
 } ;
 
+canvas.addEventListener('click', (event) => {
+    if(activeTile && !activeTile.isOccupied) {
+        buildings.push(new Building({
+            position: {
+                x: activeTile.position.x, 
+                y: activeTile.position.y
+            }
+        }))
+        activeTile.isOccupied = true;
+    }
+    console.log(buildings);
+})
+
 window.addEventListener('mousemove', (event) => {
     mouse.x = event.clientX;
     mouse.y = event.clientY;
+
+    activeTile = null;
+    for(let i = 0; i < placementTiles.length; i++){
+        const tile = placementTiles[i];
+        if(
+            mouse.x > tile.position.x &&
+            mouse.x < tile.position.x + tile.size &&
+            mouse.y > tile.position.y &&
+            mouse.y < tile.position.y + tile.size
+        ) {
+                activeTile = tile;
+                break;
+        }
+    }
 })
 
 animate();
